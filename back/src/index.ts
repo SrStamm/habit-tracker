8c9fe4ab581d04ec14a678d5f0964931e9805fca
Mirko Stamm <mirkostamm19@gmail.com>
2026-09-19 10:45:33 +0100

    feat: ligação ao mongo criado

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,4 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.configDotenv();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Ligado ao MongoDB Atlas!"))
+  .catch((erro) => console.error("Erro ao ligar:", erro));
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
