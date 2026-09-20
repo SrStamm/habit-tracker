c4b90c6626ca0ac4dfbb8f4461f84a4a34654567
Mirko Stamm <mirkostamm19@gmail.com>
2026-09-20 12:04:18 +0100

    feat: criado service e controller para o login

--- back/src/modules/auth/auth.controller.ts
+++ back/src/modules/auth/auth.controller.ts
@@ -1,8 +1,16 @@
 import { type Request, type Response } from "express";
-import { createUser } from "./auth.service";
+import { createUser, validarUsuario, firmarToken } from "./auth.service";
 
-export const handleLogin = (req: Request, res: Response) => {
+export const handleLogin = async (req: Request, res: Response) => {
   const { nome, password } = req.body;
+
+  try {
+    const user = await validarUsuario(nome, password);
+    const token = await firmarToken(user._id.toString());
+    return res.status(200).json({ token });
+  } catch (error) {
+    return res.status(400).json({ error: (error as Error).message });
+  }
 };
 
 export const handleRegister = async (req: Request, res: Response) => {
--- back/src/modules/auth/auth.service.ts
+++ back/src/modules/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { SignJWT, jwtVerify } from "jose";
 import User from "../../models/User";
 import bcrypt from "bcrypt";
 
@@ -14,4 +15,41 @@ export const createUser = async (nome: string, password: string) => {
   // Desestructura o objeto para não retornar o password
   const { password: _, ...userWithoutPassword } = saved.toObject();
   return userWithoutPassword;
+};
+
+const secret = new TextEncoder().encode(
+  process.env.JWT_SECRET ??
+    (() => {
+      throw new Error("JWT_SECRET não definido");
+    })(),
+);
+
+export const firmarToken = async (userId: string) => {
+  const token = await new SignJWT({ userId: userId })
+    .setProtectedHeader({ alg: "HS256" })
+    .setExpirationTime("1h")
+    .sign(secret);
+
+  return token;
+};
+
+export const verificarToken = async (token: string) => {
+  try {
+    const { payload } = await jwtVerify(token, secret);
+    return payload;
+  } catch {
+    throw new Error("Token inválido ou expirado");
+  }
+};
+
+export const validarUsuario = async (nome: string, password: string) => {
+  // Valida que o usuario existe
+  const existing = await User.findOne({ nome });
+  if (!existing) throw new Error("Usuario não existe");
+
+  const isMatch = await bcrypt.compare(password, existing.password);
+  if (!isMatch) throw new Error("Credenciais inválidas");
+
+  const { password: _, ...usuarioSinPassword } = existing.toObject();
+  return usuarioSinPassword;
 };
