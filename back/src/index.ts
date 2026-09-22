f613162a8d48e577b59be6bfb2a376b9f1053cc3
Mirko Stamm <mirkostamm19@gmail.com>
2026-09-22 22:00:39 +0100

    feat: criado controller e route, agregado o Router

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import authRouter from "./modules/auth/auth.routes";
 import habitRouter from "./modules/habit/habit.routes";
+import entryRouter from "./modules/entry/entry.routes";
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.use("/auth", authRouter);
 app.use("/habits", habitRouter);
+app.use("/habits", entryRouter);
 
 app.get("/api/health", (_req, res) => {
   res.json({ status: "ok" });
---
+++ back/src/modules/entry/entry.controller.ts
@@ -0,0 +1,14 @@
+import { type Response, type Request } from "express";
+import { validateUserHabit, createEntry } from "./entry.service";
+
+export const handleCreateEntry = async (req: Request, res: Response) => {
+  const { value, completed } = req.body;
+  const habitId = req.params.habitId;
+  const userId = req.userId;
+
+  try {
+    await validateUserHabit(userId, habitId);
+    const newEntry = await createEntry(userId, habitId, value, completed);
+    res.status(201).json({ newEntry });
+  } catch (error) {}
+};
