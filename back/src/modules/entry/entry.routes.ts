9999a22a76f5333a8b246e80642213ebd42c9508
Mirko Stamm <mirkostamm19@gmail.com>
2026-09-23 14:02:52 +0100

    feat: criado e implementado o controller e a routa para obter as entries

--- back/src/modules/entry/entry.routes.ts
+++ back/src/modules/entry/entry.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { validate } from "../../middleware/validate";
 import { authMiddleware } from "../../middleware/auth";
-import { EntrySchema } from "./entry.types";
-import { handleCreateEntry } from "./entry.controller";
+import { EntrySchema, GetEntriesShemas } from "./entry.types";
+import { handleCreateEntry, handleGetEntries } from "./entry.controller";
 
 const entryRouter: Router = Router();
 entryRouter.use(authMiddleware);
+
+entryRouter.get(
+  "/:habitId/entries",
+  validate(GetEntriesShemas),
+  handleGetEntries,
+);
 
 entryRouter.post("/:habitId/entries", validate(EntrySchema), handleCreateEntry);
 
