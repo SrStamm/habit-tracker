a956d3a48ed83bbec3fffe15b4849cb70b2cfd50
Mirko Stamm <mirkostamm19@gmail.com>
2026-09-23 20:05:02 +0100

    feat: criado HomePage basico e as Routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./features/home/pages/HomePage";
+import LoginPage from "./features/auth/pages/LoginPage";
+import RegisterPage from "./features/auth/pages/RegisterPage";
+
 export default function App() {
   return (
-    <div>
-      <h1>Habits Tracker</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
