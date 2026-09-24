14e4c523e4a1e1a7935b78e3a55b7ddce57a29a1
Mirko Stamm <mirkostamm19@gmail.com>
2026-09-24 13:58:28 +0100

    feat: chamada e hook para obter todas as entries, de um ou todos os habitos

--- front/src/features/entry/hooks/useEntries.ts
+++ front/src/features/entry/hooks/useEntries.ts
@@ -1,8 +1,39 @@
-import { CreateEntryDTO, Entry } from "@habits/shared/entry";
+import {
+  CreateEntryDTO,
+  Entry,
+  GetEntriesQueryDTO,
+} from "@habits/shared/entry";
 import { useState } from "react";
-import { createEntry } from "../api";
+import { createEntry, getAllEntries } from "../api";
 
-// TODO:  useEntries
+export const useEntries = () => {
+  const [data, setData] = useState<Entry[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isPending, setIsPending] = useState(false);
+
+  const mutate = async (
+    filter?: GetEntriesQueryDTO,
+    habitId?: string,
+  ): Promise<Entry[] | undefined> => {
+    if (isPending) return;
+
+    setData(null);
+    setIsPending(true);
+    setError(null);
+
+    try {
+      const result = await getAllEntries(filter, habitId);
+      setData(result.entries);
+      return result.entries;
+    } catch (e) {
+      setError((e as Error).message);
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  return { data, error, isPending, mutate };
+};
 
 export const useCreateEntry = () => {
   const [data, setData] = useState<Entry | null>(null);
